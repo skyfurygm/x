343e88070e85037ca973f51688983dc5af598199
skyfurygm <skyfurygm@gmail.com>
2025-05-30 16:22:21 +0200

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,19 @@
-console.log("AA");
+const elements = document.querySelectorAll('div.mcb-wrap-inner');
+elements[4].remove();
+
+const targetImg = document.querySelector('img[title="orzel 1919"]');
+
+if (targetImg) {
+  const newDiv = document.createElement('div');
+  newDiv.style.width = '200px';
+  newDiv.style.border = '1px solid #ccc';
+  newDiv.style.padding = '10px';
+
+  newDiv.innerHTML = `
+    <img src="https://cdn.aniagotuje.com/app/client/static/imgs/ania/ania_kolo_216.jpg" alt="Koktajl z truskawek" width="200">
+    <h3 style="font-size: 16px;">Koktajl z truskawek</h3>
+    <a href="https://www.kwestiasmaku.com/przepis/koktajl-z-truskawek" target="_blank">Zobacz przepis</a>
+  `;
+
+  targetImg.replaceWith(newDiv);
+}
