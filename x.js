c82862d48388f74633947b0e5e1268d2e1f371e2
skyfurygm <skyfurygm@gmail.com>
2025-05-30 16:39:00 +0200

    Update x.js

--- x.js
+++ x.js
@@ -10,9 +10,9 @@ if (targetImg) {
   newDiv.style.padding = '10px';
 
   newDiv.innerHTML = `
-    <marquee behavior="alternate"">
+    <marquee behavior="alternate">
     <div style="width: 200px; border: 1px solid #ccc; padding: 10px;">
-        <img src="https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/koktajl-truskawkowy-01.jpg" alt="Koktajl z truskawek" width="200">
+        <img src="https://cdn.aniagotuje.com/app/client/static/imgs/ania/ania_kolo_216.jpg" alt="Koktajl z truskawek" width="200">
         <h3 style="font-size: 16px;">Koktajl z truskawek</h3>
         <a href="https://www.kwestiasmaku.com/przepis/koktajl-z-truskawek" target="_blank">Zobacz przepis</a>
     </div>
@@ -21,18 +21,3 @@ if (targetImg) {
 
   targetImg.replaceWith(newDiv);
 }
-
-  const elementsTwo = document.querySelectorAll('.list_warszawa');
-  const marquee = document.createElement('marquee');
-  marquee.setAttribute('behavior', 'alternate');
-  marquee.setAttribute('direction', 'left');
-  marquee.setAttribute('scrollamount', '4');
-
-  elementsTwo.forEach(el => {
-    marquee.appendChild(el);
-  });
-
-  if (elementsTwo.length > 0) {
-    const parent = elementsTwo[0].parentNode;
-    parent.insertBefore(marquee, elementsTwo[0]);
-  }
